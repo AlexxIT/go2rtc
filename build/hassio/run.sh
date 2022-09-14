d72d7b089cb566634ba72a8f8d5e6297c800adb6
Alexey Khit <alexey.khit@gmail.com>
2022-09-14 14:12:43 +0300

    Reduce docker container size and add python inside

--- build/hassio/run.sh
+++ build/hassio/run.sh
@@ -2,23 +2,13 @@
 
 set +e
 
-# add the feature for update to any version
-if [ -f "/config/go2rtc.version" ]; then
-    branch=`cat /config/go2rtc.version`
-    echo "Update to version $branch"
-    git clone --depth 1 --branch "$branch" https://github.com/AlexxIT/go2rtc \
-      && cd go2rtc \
-      && CGO_ENABLED=0 go build -ldflags "-s -w" -trimpath -o /usr/local/bin \
-      && rm -r /go2rtc && rm /config/go2rtc.version
-fi
-
-# set cwd for go2rtc (for config file, Hass itegration, etc)
+# set cwd for go2rtc (for config file, Hass integration, etc)
 cd /config
 
 # add the feature to override go2rtc binary from Hass config folder
 export PATH="/config:$PATH"
 
 while true; do
-    go2rtc
-    sleep 5
+  go2rtc
+  sleep 5
 done
