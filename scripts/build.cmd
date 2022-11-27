4915f12bdefd7be9444dd7e04de69e917a723121
Alexey Khit <alexey.khit@gmail.com>
2022-11-27 09:57:04 +0300

    Add build for win arm64

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -10,6 +10,11 @@ go build -ldflags "-s -w" -trimpath && 7z a -sdel %FILENAME% go2rtc.exe
 @SET FILENAME=go2rtc_win32.zip
 go build -ldflags "-s -w" -trimpath && 7z a -sdel %FILENAME% go2rtc.exe
 
+@SET GOOS=windows
+@SET GOARCH=arm64
+@SET FILENAME=go2rtc_win_arm64.zip
+go build -ldflags "-s -w" -trimpath && 7z a -sdel %FILENAME% go2rtc.exe
+
 @SET GOOS=linux
 @SET GOARCH=amd64
 @SET FILENAME=go2rtc_linux_amd64
