a36359f3ddd554c6d7a5bc132d5dab1cb12a7c36
Sergey Krashevich <svk@svk.su>
2023-01-20 18:38:46 +0300

    Update api.go

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -118,6 +118,7 @@ func middlewareCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization")
 		next.ServeHTTP(w, r)
 	})
 }
