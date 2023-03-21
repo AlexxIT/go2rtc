729ba28e714e153cfffb7e134a66e8462e06d943
Sergey Krashevich <svk@svk.su>
2023-03-21 14:17:09 +0300

    Refactor CSS styles for improved readability and consistency across pages

--- www/main.js
+++ www/main.js
@@ -41,6 +41,47 @@ nav a:hover {
 nav li {
     display: inline;
 }
+.button-container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 20px;
+    margin: 20px 0;
+}
+
+button {
+    font-family: 'Poppins', Arial, Helvetica, sans-serif;
+    background-color: rgba(177, 152, 145, .3);
+    color: #695753;
+    font-weight: 600;
+    font-size: 14px;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+
+button:hover {
+    background-color: #b19891;
+}
+form input[type="submit"] {
+    font-family: 'Poppins', Arial, Helvetica, sans-serif;
+    background-color: rgba(177, 152, 145, .3);
+    color: #695753;
+    font-weight: 600;
+    font-size: 14px;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+
+form input[type="submit"]:hover {
+    background-color: #b19891;
+}
 </style>
 <nav>
     <ul>
