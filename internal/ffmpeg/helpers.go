490a48cd50876ba5101e0b3afef28e2bff6b1bbc
Sergey Krashevich <svk@svk.su>
2023-07-11 10:35:53 +0300

    Refactored code to resize JPEG snapshot if "h" parameter exists in the URL query

--- internal/ffmpeg/helpers.go
+++ internal/ffmpeg/helpers.go
@@ -2,11 +2,17 @@ package ffmpeg
 
 import (
 	"bytes"
+	"fmt"
 	"os/exec"
 )
 
-func TranscodeToJPEG(b []byte) ([]byte, error) {
-	cmd := exec.Command(defaults["bin"], "-hide_banner", "-i", "-", "-f", "mjpeg", "-")
+func TranscodeToJPEG(b []byte, height ...int) ([]byte, error) {
+	cmdArgs := []string{defaults["bin"], "-hide_banner", "-i", "-", "-f", "mjpeg"}
+	if len(height) > 0 {
+		cmdArgs = append(cmdArgs, "-vf", fmt.Sprintf("scale=-1:%d", height[0]))
+	}
+	cmdArgs = append(cmdArgs, "-")
+	cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
 	cmd.Stdin = bytes.NewBuffer(b)
 	return cmd.Output()
 }
