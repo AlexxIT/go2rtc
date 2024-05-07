8840a18cdd921490bb6d91376ed95dd3f25f3dc4
Sergey Krashevich <svk@svk.su>
2024-05-07 15:30:46 +0300

    fix(entrypoint): streamline config initialization and execution
    Simplify the entrypoint script by removing the creation of a default configuration file if it doesn't exist. Instead, directly pass the default API listen port configuration to the `go2rtc` command, while still allowing custom configurations through the existing `CONFIG_PATH`. This change ensures that the service can start with default settings without needing to write them to a file, simplifying deployment and configuration management.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,8 @@
 CONFIG_PATH=${CONFIG_PATH:-/config/go2rtc.yaml}
 PORT=${PORT:-1984}
 
-# Create the config file with default settings if it doesn't exist
 if [ ! -f "$CONFIG_PATH" ]; then
     mkdir -p "$(dirname "$CONFIG_PATH")"
-    echo "api:" > "$CONFIG_PATH"
-    echo -e "\tlisten: ':$PORT'" >> "$CONFIG_PATH"
 fi
 
-exec go2rtc -config "$CONFIG_PATH" "$@"
+exec go2rtc -config "{ api: { listen: \":$PORT\" } }" -config "$CONFIG_PATH" "$@"
