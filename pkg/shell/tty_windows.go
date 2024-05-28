cc74504ed812df848e00fad26c6660ca3df1978c
Sergey Krashevich <svk@svk.su>
2024-05-28 10:16:48 +0300

    feat(shell): add Windows support for TTY detection

---
+++ pkg/shell/tty_windows.go
@@ -0,0 +1,19 @@
+//go:build windows
+
+package shell
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+var (
+	kernel32           = syscall.NewLazyDLL("kernel32.dll")
+	procGetConsoleMode = kernel32.NewProc("GetConsoleMode")
+)
+
+func IsInteractive(fd uintptr) bool {
+	var st uint32
+	r, _, e := syscall.Syscall(procGetConsoleMode.Addr(), 2, fd, uintptr(unsafe.Pointer(&st)), 0)
+	return r != 0 && e == 0
+}
