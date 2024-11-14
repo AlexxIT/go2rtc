c4e31e1d22f51fbdf6a71b833065bf161836e40e
lrobot <lrobot.qq@gmail.com>
2024-11-14 18:50:52 +0800

    fix issue 1458: empty stream got incoming brower webrtc producer can not be consumer by other webpage

--- internal/streams/add_consumer.go
+++ internal/streams/add_consumer.go
@@ -23,7 +23,8 @@ func (s *Stream) AddConsumer(cons core.Consumer) (err error) {
 	producers:
 		for prodN, prod := range s.producers {
 			// check for loop request, ex. `camera1: ffmpeg:camera1`
-			if info, ok := cons.(core.Info); ok && prod.url == info.GetSource() {
+			
+			if info, ok := cons.(core.Info); ok && prod.url == info.GetSource() && prod.url != "" {
 				log.Trace().Msgf("[streams] skip cons=%d prod=%d", consN, prodN)
 				continue
 			}
