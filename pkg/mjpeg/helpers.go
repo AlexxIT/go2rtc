55af09a3502ad72b3909df637b03740321d7c404
Alex X <alexey.khit@gmail.com>
2025-01-05 11:03:44 +0300

    Add support fix JPEG from some MJPEG sources

--- pkg/mjpeg/helpers.go
+++ pkg/mjpeg/helpers.go
@@ -9,24 +9,38 @@ import (
 	"github.com/pion/rtp"
 )
 
-// FixJPEG - reencode JPEG if it has wrong header
-//
-// for example, this app produce "bad" images:
-// https://github.com/jacksonliam/mjpg-streamer
-//
-// and they can't be uploaded to the Telegram servers:
-// {"ok":false,"error_code":400,"description":"Bad Request: IMAGE_PROCESS_FAILED"}
 func FixJPEG(b []byte) []byte {
 	// skip non-JPEG
-	if len(b) < 10 || b[0] != 0xFF || b[1] != 0xD8 {
-		return b
-	}
-	// skip if header OK for imghdr library
-	// https://docs.python.org/3/library/imghdr.html
-	if string(b[2:4]) == "\xFF\xDB" || string(b[6:10]) == "JFIF" || string(b[6:10]) == "Exif" {
+	if len(b) < 10 || b[0] != 0xFF || b[1] != markerSOI {
 		return b
 	}
 
+	// skip JPEG without app marker
+	if b[2] == 0xFF && b[3] == markerDQT {
+		return b
+	}
+
+	switch string(b[6:10]) {
+	case "JFIF", "Exif":
+		// skip if header OK for imghdr library
+		// - https://docs.python.org/3/library/imghdr.html
+		return b
+	case "AVI1":
+		// adds DHT tables to JPEG file before SOS marker
+		// useful when you want to save a JPEG frame from an MJPEG stream
+		// - https://github.com/image-rs/jpeg-decoder/issues/76
+		// - https://github.com/pion/mediadevices/pull/493
+		// - https://bugzilla.mozilla.org/show_bug.cgi?id=963907#c18
+		return InjectDHT(b)
+	}
+
+	// reencode JPEG if it has wrong header
+	//
+	// for example, this app produce "bad" images:
+	// https://github.com/jacksonliam/mjpg-streamer
+	//
+	// and they can't be uploaded to the Telegram servers:
+	// {"ok":false,"error_code":400,"description":"Bad Request: IMAGE_PROCESS_FAILED"}
 	img, err := jpeg.Decode(bytes.NewReader(b))
 	if err != nil {
 		return b
@@ -53,4 +67,27 @@ func Encoder(codec *core.Codec, handler core.HandlerFunc) core.HandlerFunc {
 		clone.Payload = buf.Bytes()
 		handler(&clone)
 	}
+}
+
+const dhtSize = 432 // known size for 4 default tables
+
+func InjectDHT(b []byte) []byte {
+	if bytes.Index(b, []byte{0xFF, markerDHT}) > 0 {
+		return b // already exist
+	}
+
+	i := bytes.Index(b, []byte{0xFF, markerSOS})
+	if i < 0 {
+		return b
+	}
+
+	dht := make([]byte, 0, dhtSize)
+	dht = MakeHuffmanHeaders(dht)
+
+	tmp := make([]byte, len(b)+dhtSize)
+	copy(tmp, b[:i])
+	copy(tmp[i:], dht)
+	copy(tmp[i+dhtSize:], b[i:])
+
+	return tmp
 }
