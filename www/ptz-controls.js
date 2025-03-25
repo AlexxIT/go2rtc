fa1bbaf40e6ab141f9134d62bb62e2ee41449bb5
Julian <info@jcsoft.dev>
2025-03-25 10:13:00 +0000

    Improve Ui icon for PTZ controls

--- www/ptz-controls.js
+++ www/ptz-controls.js
@@ -1,4 +1,4 @@
-const defaultIcon = `<div style="color: rgb(255, 255, 255);"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M15.54 5.54 13.77 7.3 12 5.54 10.23 7.3 8.46 5.54 12 2zm2.92 10-1.76-1.77L18.46 12l-1.76-1.77 1.76-1.77L22 12zm-10 2.92 1.77-1.76L12 18.46l1.77-1.76 1.77 1.76L12 22zm-2.92-10 1.76 1.77L5.54 12l1.76 1.77-1.76 1.77L2 12z"></path><circle cx="12" cy="12" r="3"></circle></svg></div>`;
+const defaultIcon = `<div style="color: rgb(255, 255, 255);margin-top: 3px;"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M15.54 5.54 13.77 7.3 12 5.54 10.23 7.3 8.46 5.54 12 2zm2.92 10-1.76-1.77L18.46 12l-1.76-1.77 1.76-1.77L22 12zm-10 2.92 1.77-1.76L12 18.46l1.77-1.76 1.77 1.76L12 22zm-2.92-10 1.76 1.77L5.54 12l1.76 1.77-1.76 1.77L2 12z"></path><circle cx="12" cy="12" r="3"></circle></svg></div>`;
 
 class PTZControls extends HTMLElement {
   constructor() {
@@ -191,7 +191,7 @@ class PTZControls extends HTMLElement {
       const controlsPanel = this.shadowRoot.querySelector(".controls");
       const toggleButton = this.shadowRoot.querySelector(".toggle-button");
       controlsPanel.classList.remove("visible");
-      toggleButton.textContent = "⟨";
+      toggleButton.innerHTML = defaultIcon;
       this.controlsVisible = false;
     }, 5000);
   }
