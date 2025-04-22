6df1e68a5fac7be6f1dd91f22cd7a20ac43afa5b
Alex X <alexey.khit@gmail.com>
2025-04-22 10:26:00 +0300

    Update wyoming producer and backchannel

--- pkg/wyoming/wyoming.go
+++ pkg/wyoming/wyoming.go
@@ -18,25 +18,9 @@ func Dial(rawURL string) (core.Producer, error) {
 		return nil, err
 	}
 
-	cc := core.Connection{
-		ID:         core.NewID(),
-		FormatName: "wyoming",
-		Medias: []*core.Media{
-			{
-				Kind: core.KindAudio,
-				Codecs: []*core.Codec{
-					{Name: core.CodecPCML, ClockRate: 16000},
-				},
-			},
-		},
-		Transport: conn,
-	}
-
 	if u.Query().Get("backchannel") != "1" {
-		cc.Medias[0].Direction = core.DirectionRecvonly
-		return &Producer{cc, NewAPI(conn)}, nil
+		return newProducer(conn), nil
 	} else {
-		cc.Medias[0].Direction = core.DirectionSendonly
-		return &Backchannel{cc, NewAPI(conn)}, nil
+		return newBackchannel(conn), nil
 	}
 }
