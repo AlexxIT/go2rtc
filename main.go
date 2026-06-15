4d276b15bd32d4f6de6d2f118a170ae1848977a9
VaBanck <75133860+VaBanck@users.noreply.github.com>
2026-06-15 23:48:46 +0300

    Update main.go

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ import (
 
 func main() {
 	// version will be set later from -buildvcs info, this used only as fallback
-	app.Version = "1.9.14"
+	app.Version = "1.9.17"
 
 	type module struct {
 		name string
