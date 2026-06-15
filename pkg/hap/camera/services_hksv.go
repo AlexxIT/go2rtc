e3aff3fe616c3bbd8f8c2b4355fb3207e352d201
VaBanck <75133860+VaBanck@users.noreply.github.com>
2026-06-15 23:21:28 +0300

    Update services_hksv.go

--- pkg/hap/camera/services_hksv.go
+++ pkg/hap/camera/services_hksv.go
@@ -97,7 +97,6 @@ func ServiceCameraEventRecordingManagement() *hap.Service {
 						Channels:        1,
 						BitrateMode:     []byte{AudioCodecBitrateVariable},
 						SampleRate:      []byte{AudioRecordingSampleRate24Khz, AudioRecordingSampleRate32Khz, AudioRecordingSampleRate48Khz},
-						MaxAudioBitrate: []uint32{64},
 					},
 				},
 			},
