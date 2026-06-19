687da40164c95b12dcfea276b5b6d7e0e3d11488
VaBanck <75133860+VaBanck@users.noreply.github.com>
2026-06-19 19:31:10 +0300

    Update playback_linux.go

--- pkg/alsa/playback_linux.go
+++ pkg/alsa/playback_linux.go
@@ -3,9 +3,9 @@ package alsa
 import (
 	"fmt"
 
-	"github.com/AlexxIT/go2rtc/pkg/alsa/device"
-	"github.com/AlexxIT/go2rtc/pkg/core"
-	"github.com/AlexxIT/go2rtc/pkg/pcm"
+	"github.com/VaBanck/go2rtc/pkg/alsa/device"
+	"github.com/VaBanck/go2rtc/pkg/core"
+	"github.com/VaBanck/go2rtc/pkg/pcm"
 	"github.com/pion/rtp"
 )
 
