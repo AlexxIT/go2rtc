7aaa07567369b363031eb399a5b483d16dc60806
VaBanck <75133860+VaBanck@users.noreply.github.com>
2026-06-19 19:17:55 +0300

    Update aac.go

--- pkg/aac/aac.go
+++ pkg/aac/aac.go
@@ -4,8 +4,8 @@ import (
 	"encoding/hex"
 	"fmt"
 
-	"github.com/AlexxIT/go2rtc/pkg/bits"
-	"github.com/AlexxIT/go2rtc/pkg/core"
+	"github.com/VaBanck/go2rtc/pkg/bits"
+	"github.com/VaBanck/go2rtc/pkg/core"
 )
 
 const (
