ea5db272aad78fb9da2cf77ec426855703b99ec8
VaBanck <75133860+VaBanck@users.noreply.github.com>
2026-06-19 19:29:24 +0300

    Update adts.go

--- pkg/aac/adts.go
+++ pkg/aac/adts.go
@@ -3,8 +3,8 @@ package aac
 import (
 	"encoding/hex"
 
-	"github.com/AlexxIT/go2rtc/pkg/bits"
-	"github.com/AlexxIT/go2rtc/pkg/core"
+	"github.com/VaBanck/go2rtc/pkg/bits"
+	"github.com/VaBanck/go2rtc/pkg/core"
 	"github.com/pion/rtp"
 )
 
