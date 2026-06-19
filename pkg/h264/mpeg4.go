a8ae9c6b26c573ed5f2f917dcad230a7e1f1d3a7
VaBanck <75133860+VaBanck@users.noreply.github.com>
2026-06-19 19:32:21 +0300

    Update mpeg4.go

--- pkg/h264/mpeg4.go
+++ pkg/h264/mpeg4.go
@@ -7,7 +7,7 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 
-	"github.com/AlexxIT/go2rtc/pkg/core"
+	"github.com/VaBanck/go2rtc/pkg/core"
 )
 
 // DecodeConfig - extract profile, SPS and PPS from MPEG4 config
